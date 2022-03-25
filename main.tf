16c81fb095129087372294493356173e69e082b7
ragog <giovanni@checklyhq.com>
2022-03-25 13:47:38 +0100

    refactor: update args

--- main.tf
+++ main.tf
@@ -113,10 +113,10 @@ resource "checkly_dashboard" "example-dashboard" {
 
 resource "checkly_maintenance_windows" "maintenance-1" {
   name            = "Maintenance Windows"
-  starts_at       = "2014-08-24T00:00:00.000Z"
-  ends_at         = "2014-08-25T00:00:00.000Z"
+  starts_at       = "2022-04-24T00:00:00.000Z"
+  ends_at         = "2022-04-25T00:00:00.000Z"
   repeat_unit     = "MONTH"
-  repeat_ends_at  = "2014-08-24T00:00:00.000Z"
+  repeat_ends_at  = "2023-08-24T00:00:00.000Z"
   repeat_interval = 1
   tags = [
     "auto",
