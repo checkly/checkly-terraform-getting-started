e48ca21d89af06dca438896e7e59b4cda5b76031
ragog <giovanni@checklyhq.com>
2024-06-28 13:59:53 +0200

    feat: add multistep, update browser check code

--- main.tf
+++ main.tf
@@ -70,16 +70,85 @@ resource "checkly_check" "browser-check-1" {
   group_id = checkly_check_group.group-1.id
 
   script = <<EOT
-const assert = require("chai").assert;
-const { chromium } = require("playwright");
+/**
+  * To learn more about Playwright Test visit:
+  * https://checklyhq.com/docs/browser-checks/playwright-test/
+  * https://playwright.dev/docs/writing-tests
+  */
 
-const browser = await chromium.launch();
-const page = await browser.newPage();
-await page.goto("https://danube-web.shop");
-const title = await page.title();
+const { expect, test } = require('@playwright/test')
 
-assert.equal(title, "Google");
-await browser.close();
+// Configure the Playwright Test timeout to 210 seconds,
+// ensuring that longer tests conclude before Checkly's browser check timeout of 240 seconds.
+// The default Playwright Test timeout is set at 30 seconds.
+// For additional information on timeouts, visit: https://checklyhq.com/docs/browser-checks/timeouts/
+test.setTimeout(210000)
+
+// Set the action timeout to 10 seconds to quickly identify failing actions.
+// By default Playwright Test has no timeout for actions (e.g. clicking an element).
+test.use({ actionTimeout: 10000 })
+
+test('visit page and take screenshot', async ({ page }) => {
+  // Change checklyhq.com to your site's URL,
+  // or, even better, define a ENVIRONMENT_URL environment variable
+  // to reuse it across your browser checks
+  const response = await page.goto(process.env.ENVIRONMENT_URL || 'https://checklyhq.com/welcome')
+
+  // Take a screenshot
+  await page.screenshot({ path: 'screenshot.jpg' })
+
+  // Test that the response did not fail
+  expect(response.status(), 'should respond with correct status code').toBeLessThan(400)
+})
+
+EOT
+}
+
+resource "checkly_check" "multistep-check-1" {
+  name      = "Example multistep check"
+  type      = "MULTI_STEP"
+  activated = true
+  frequency = 10
+
+  retry_strategy {
+    type                 = "FIXED"
+    base_backoff_seconds = 60
+    max_duration_seconds = 600
+    max_retries          = 3
+    same_region          = false
+  }
+
+  locations = [
+    "us-west-1",
+    "eu-central-1"
+  ]
+
+  group_id = checkly_check_group.group-1.id
+
+  script = <<EOT
+import { test, expect } from '@playwright/test';
+
+const headers = {
+	Accept: 'application/json',
+};
+
+test('login', async ({ request }) => {
+	let itemList;
+	let itemId;
+
+	await test.step('GET todos', async () => {
+		const response = await request.get(`https://jsonplaceholder.typicode.com/todos`, { headers });
+		itemList = await response.json();
+		itemId = await itemList[0].userId;
+	});
+
+	await test.step('GET todo', async () => {
+        const response = await request.get('https://jsonplaceholder.typicode.com/todos/' + itemId, { headers });
+		const item = await response.json()
+	    expect(item.completed).toBeFalsy();
+    });
+
+});
 
 EOT
 }
